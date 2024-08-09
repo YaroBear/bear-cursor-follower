5e586e6e0a03ed1d2dfcc43d4e6b0fcc1a2c394a
Yaroslav Berejnoi <berejnoi.yaroslav@gmail.com>
2024-08-09 08:18:09 -0600

    impl cursor following

--- index.js
+++ index.js
@@ -1,39 +1,70 @@
-const calcAngleDegrees = (x, y) => {
-    return (Math.atan2(y, x) * 180) / Math.PI;
+const convertToDegrees = (angle) => {
+    return (angle * 180) / Math.PI;
 };
 
+const calcAngleRadians = (x, y) => {
+    return Math.atan2(y, x);
+};
+
+const lerp = (a, b, alpha) => {
+    return a + alpha * (b - a);
+};
+
+const getRelativeAngle = (bear, mouseEvent) => {
+    const rect = bear.getBoundingClientRect();
+
+    const y = rect.y - mouseEvent.clientY;
+    const x = mouseEvent.clientX - rect.x;
+
+    return calcAngleRadians(x, y);
+};
+
+const lookAtCursor = (relativeAngle) => {
+    const abs_angle = Math.abs(relativeAngle);
+
+    if (abs_angle < 45) {
+        bear.className = "bear";
+        bear.classList.add("right");
+    }
+    if (abs_angle > 135) {
+        bear.className = "bear";
+        bear.classList.add("left");
+    }
+    if (relativeAngle < -45 && relativeAngle > -135) {
+        bear.className = "bear";
+        bear.classList.add("down");
+    }
+    if (relativeAngle > 45 && relativeAngle < 135) {
+        bear.className = "bear";
+        bear.classList.add("up");
+    }
+};
+
+const magnitude = 0.8;
+
+function step(timeStamp) {
+    let x = Number(bear.style.left.replace("px", ""));
+    let y = Number(bear.style.top.replace("px", ""));
+
+    x += magnitude * Math.cos(angleRad);
+    y -= magnitude * Math.sin(angleRad);
+
+    bear.style.left = `${x}px`;
+    bear.style.top = `${y}px`;
+
+    window.requestAnimationFrame(step);
+}
+
+let bear;
+let angleRad = 0;
+
 window.addEventListener("DOMContentLoaded", () => {
-    const bear = document.getElementById("bear");
+    bear = document.getElementById("bear");
 
     document.onmousemove = (e) => {
-        const rect = bear.getBoundingClientRect();
-
-        const y = rect.y - e.clientY;
-        const x = rect.x - e.clientX;
-
-        const angle = Math.abs(calcAngleDegrees(x, y));
-
-        console.log(angle);
-
-        if ((angle > 0 && angle < 45) || (angle > 320 && angle < 365)) {
-            bear.className = "";
-            const classes = ["bear", "right"];
-            bear.classList.add(...classes);
-        }
-        if (angle > 135 && angle < 225) {
-            bear.className = "";
-            const classes = ["bear", "left"];
-            bear.classList.add(...classes);
-        }
-        if (angle > 225 && angle < 320) {
-            bear.className = "";
-            const classes = ["bear", "down"];
-            bear.classList.add(...classes);
-        }
-        if (angle > 45 && angle < 135) {
-            bear.className = "";
-            const classes = ["bear", "up"];
-            bear.classList.add(...classes);
-        }
+        angleRad = getRelativeAngle(bear, e);
+        lookAtCursor(convertToDegrees(angleRad));
     };
+
+    window.requestAnimationFrame(step);
 });
